afe9cd20edb0b33a1de2e2d1b4088f819a4640fe
Sandra Wasiela <sandra.wasiela@interia.pl>
2020-01-16 22:56:19 +0100

    Ekran startowy i pierwsze okno

--- app/src/main/java/com/app/findLowGI/MainActivity.java
+++ app/src/main/java/com/app/findLowGI/MainActivity.java
@@ -2,23 +2,53 @@ package com.app.findLowGI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-        private String food="banana";
+        private String food = "banana";
+        private static int SPLASH_TIME_OUT = 4000;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+            setContentView(R.layout.second_view);
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+
+                }
+            }, SPLASH_TIME_OUT);
+
+            //button
+            Button findButton = (Button) findViewById(R.id.findButton);
+            findButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    EditText productName = (EditText) findViewById(R.id.productName);
+                }
+            });
+
+
+
+
+            // pobieranie danych z API
 
             try {
 
                 int wynik =new DataDownload().execute(food).get();
 
-                Log.d("glikemia","osstateczny wynik:"+Integer.toString(wynik));
+                Log.d("glikemia","ostateczny wynik:"+Integer.toString(wynik));
 
             }catch (Exception e)
             {
