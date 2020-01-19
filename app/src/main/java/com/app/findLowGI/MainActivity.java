555eca37f773e353ed62b814458e216e2c356df5
Sandra Wasiela <sandra.wasiela@interia.pl>
2020-01-19 22:27:23 +0100

    Poprawki, zaczytanie produktu wprowadzonego w pole przez użytkownika

--- app/src/main/java/com/app/findLowGI/MainActivity.java
+++ app/src/main/java/com/app/findLowGI/MainActivity.java
@@ -6,9 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import java.util.HashMap;
 import java.util.Iterator;
@@ -16,57 +13,24 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-        private String food = "banana";
-        private static int SPLASH_TIME_OUT = 4000;
+
+        private static int SPLASH_TIME_OUT = 5000;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.second_view);
+            setContentView(R.layout.activity_home);
 
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                    Intent homeIntent = new Intent(MainActivity.this, SecondActivity.class);
                     startActivity(homeIntent);
                     finish();
 
                 }
             }, SPLASH_TIME_OUT);
 
-            //button
-            Button findButton = (Button) findViewById(R.id.findButton);
-            findButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    EditText productName = (EditText) findViewById(R.id.productName);
-                }
-            });
-
-
-
-
-            // pobieranie danych z API
-
-            try {
-
-                HashMap results =new DataDownload().execute(food).get();
-
-                Iterator hmIterator = results.entrySet().iterator();
-
-
-                while (hmIterator.hasNext()) {
-                    Map.Entry mapElement = (Map.Entry) hmIterator.next();
-
-                    Log.d("glikemia",mapElement.getKey()+": "+mapElement.getValue());
-
-                }
-
-
-            }catch (Exception e)
-            {
-                Log.d("glikemia",e.toString());
-            }
 
 
         }
---
+++ app/src/main/java/com/app/findLowGI/SecondActivity.java
@@ -0,0 +1,59 @@
+package com.app.findLowGI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private String food;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_view);
+
+        // button
+        Button findButton = (Button) findViewById(R.id.findButton);
+        findButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText productName = (EditText) findViewById(R.id.productName);
+                food = productName.getText().toString();
+
+                try {
+
+                    // pobieranie danych z API
+
+                    HashMap results = new DataDownload().execute(food).get();
+
+                    Iterator hmIterator = results.entrySet().iterator();
+
+
+                    while (hmIterator.hasNext()) {
+                        Map.Entry mapElement = (Map.Entry) hmIterator.next();
+
+                        Log.d("glikemia", mapElement.getKey() + ": " + mapElement.getValue());
+
+                    }
+
+
+                } catch (Exception e) {
+                    Log.d("glikemia", e.toString());
+                }
+            }
+        });
+
+
+
+
+    }
+}
+
